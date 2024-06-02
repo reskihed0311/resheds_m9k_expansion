74694e9aeb2d6fb766bf29fbbff547b520522194
NotTrashraD <bellie1579@gmail.com>
2024-06-02 20:43:34 +0200

    initial commit

---
+++ lua/autorun/rshd_killicons.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile()
+
+local icol = Color( 255, 255, 255, 255 ) 
+if CLIENT then
+	killicon.Add( "m9k_rshd_p99", "vgui/hud/m9k_rshd_p99", icol  )
+end
---
+++ lua/weapons/rshd_m9k_soundscript.lua
@@ -0,0 +1,174 @@
+AddCSLuaFile()
+
+
+
+
+
+
+
+// P99
+sound.Add({
+	name = 			"p99.rshd_Fire",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/p99/fire_fp.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.P99_clipOut",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/p99/magout.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.P99_clipHit",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/p99/maghit.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.P99_clipIn",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/p99/magIn.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.P99_slideRel",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/p99/sliderel.wav"}
+})
+
+
+// W1983
+
+
+sound.Add({
+	name = 			"m1873.rshd_Fire",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/m1873/fire_fp_2.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.W1873_leverDown",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/m1873/leverdown.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.W1873_leverUp",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/m1873/leverup.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.W1873_shellLoop",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/m1873/shell.wav"}
+})
+
+
+// AK-5C
+
+sound.Add({
+	name = 			"ak5c.rshd_Fire",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/ak5c/fire_fp.wav"}
+})
+
+
+sound.Add({
+	name = 			"rshd.AK5C_magOut",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/ak5c/magout.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.AK5C_magIn",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/ak5c/magin.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.AK5C_magTap",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/ak5c/magslap.wav"}
+})
+
+
+sound.Add({
+	name = 			"rshd.AK5C_slideRel",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/ak5c/boltfwd.wav"}
+})
+
+
+// SPEAR
+
+
+sound.Add({
+	name = 			"spear.rshd_Fire",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/fire_fp.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.SPEAR_magOut",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/magout_empty.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.SPEAR_magIn",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/maghit.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.SPEAR_magHit",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/magin.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.SPEAR_magTap",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/magslap.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.SPEAR_boltBack",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/pullback.wav"}
+})
+
+sound.Add({
+	name = 			"rshd.SPEAR_boltFwd",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = 			{"weapons/m9kreshed/spear/pullfwd.wav"}
+})
+
+
+
+
+
+
